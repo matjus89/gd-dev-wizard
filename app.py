82c36b493c2a98f2e1d63b8c07703cad0d5f2f08
matjus89 <65369401+matjus89@users.noreply.github.com>
2021-05-05 02:34:49 +0200

    final url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, url_for, request, jsonify, redirect
 from classes_and_functions import *
 
 
